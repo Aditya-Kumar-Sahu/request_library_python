0a2fd03ff8fccbcb3ca06b25a45418cfab4135b8
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-10-30 13:17:01 +0530

    content

--- script.py
+++ script.py
@@ -1,13 +1,14 @@
 # the GET request
+import http
 import requests
 
-requests.get("https://api.github.com")
+# requests.get("https://api.github.com")
 #-----------------------------------------------
 
 
 
 # Response
-response = requests.get("https://api.github.com")
+# response = requests.get("https://api.github.com")
 # print(response)
 #------------------------------------------------
 
@@ -32,19 +33,35 @@ response = requests.get("https://api.github.com")
 
 
 # small code 1
-# import requests
-from requests.exceptions import HTTPError
+# # import requests
+# from requests.exceptions import HTTPError
+
+# for url in ["https://api.github.com", "https://api.github.com/invalid"]:
+#     try:
+#         response = requests.get(url)
+
+#         # If the reesponse was successful, no Exception will be raised
+#         response.raise_for_status()
+#     except HTTPError as http_err:
+#         print(f"HTTP error occurred: {http_err}")   # Python 3.6
+#     except Exception as err:
+#         print(f"Other error occured: {err}")    # Python 3.6
+#     else:
+#         print('Success!')
+#--------------------------------------------------
 
 
-for url in ["https://api.github.com", "https://api.github.com/invalid"]:
-    try:
-        response = requests.get(url)
 
-        # If the reesponse was successful, no Exception will be raised
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")   # Python 3.6
-    except Exception as err:
-        print(f"Other error occured: {err}")    # Python 3.6
-    else:
-        print('Success!')
+# Content
+response = requests.get("https://api.github.com")
+print(response.content)
+print("\n")
+
+print(response.text)
+print("\n")
+
+response.encoding = "utf-8"     # Optional: requests infer this internally
+print(response.text)
+print("\n")
+
+print(response.json())
