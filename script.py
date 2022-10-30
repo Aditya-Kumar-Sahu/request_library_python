911344b2e18f0320e7b5bf5db01f9c4124a27e91
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-10-30 11:06:04 +0530

    status codes

--- script.py
+++ script.py
@@ -3,8 +3,26 @@ import requests
 
 url = "https://api.github.com"
 requests.get(url)
+#-----------------------------------------------
 
 
 # Response
 response = requests.get(url)
-print(response)
+# print(response)
+#------------------------------------------------
+
+
+# Status Codes
+# print(response.status_code)
+
+
+if response.status_code == 200:
+    print("Success!")
+elif response.status_code == 400:
+    print("Not found. ")
+
+
+if response:
+    print("Success!")
+else:
+    print('An error has occured.')
